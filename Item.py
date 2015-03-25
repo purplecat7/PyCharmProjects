75f6124a88b012c0e80c4cea8c12cd43fa7cdd41
ewanpinnington <ewan.pinnington@gmail.com>
2015-03-25 15:17:21 +0000

    itemmm

--- Item.py
+++ Item.py
@@ -1,9 +1,12 @@
+import numpy as np
+
 class Item():
     """An Item class for use in LibrarySystem.
     """
     def __init__(self, title, ident):
         self.title = title
         self.identity = ident
+        self.checkout_date = float('NaN')
         
     def SetCheckoutDate(self, date):
         """Given a date sets the checkout date of item.
@@ -24,6 +27,17 @@ class Book(Item):
     """
     Item.finerate = 0.50 #50p/day
     Item.loantime = 4 # 4weeks
+    
+    def Fine(self, date):
+        """Given a date as a float (0-N), returns the amount of money due in 
+        fines on item.
+        """
+        if date - self.checkout_date <= 4:
+            return 0.
+        elif date - self.checkout_date > 4:
+            return self.finerate * (date - self.checkout_date - 4)
+        else:
+            raise Exception('Item aint in Itemlist fool!')
 
         
     
