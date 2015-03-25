8528331762f7137b685c40a09f8501713ed3bcad
rsmith31 <r.smith3@pgr.reading.ac.uk>
2015-03-25 14:45:29 +0000

    Added function to create new item

--- ItemManager.py
+++ ItemManager.py
@@ -1,10 +1,33 @@
 __author__ = 'rsmith31'
 
+import Item
+import library
+
 
 class ItemManager:
+    """
+    Class that handles the creation of new items
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, library_controller):
+        self.library_controller = library_controller
 
     def __del__(self):
         pass
+
+    def create_book(self, title, ident):
+        """
+        Creates a new 'book' object that will be given to the library controller to add to the item list
+
+        :param title: title of the book to be created
+        :param ident: id of the book to be created
+        :return new_book: the book object that has been created
+        """
+
+        new_book = Book(title, ident)
+
+        self.library_controller.additem(new_book)
+
+        return new_book
+
+
