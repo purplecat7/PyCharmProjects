4ccda67cea7ff577ef26401ae72223632fe55df5
CheeseFace <S.E.George@reading.ac.uk>
2015-03-25 23:18:42 +0000

    Adjusted main with real titles

--- Main.py
+++ Main.py
@@ -8,43 +8,61 @@ import UserManager
 # AUTHOR: seg
 #-
 
-def startup():
-    # Nomenclature
-    #   Item ID from #0
-    #   User ID from #1000
+class numbID:
+   idnumber = 0
 
-    libcon = library.LibraryController()
+   def __init__(self):
+      pass
 
+   def newID(self):
+      numbID.idnumber += 1
+      return numbID.idnumber 
+
+
+def librarycatalogue(libcon,infile):
+
+    idnumb = numbID()
     itemmanager = ItemManager.ItemManager()
     itemmanager.set_library_controller(libcon)
-    itemmanager.create_book('Cheese Recipes', 0)
-    itemmanager.create_book('Beef Recipes', 1)
+    file=open(infile,'r')
+    for line in file:
+      itemmanager.create_book(line.strip(), idnumb.newID())
+    print 'Number of books added: ',idnumb.idnumber
 
+def librarymembers(libcon):
+
+    idnumb = numbID()
     usermanager = UserManager.UserManager()
     usermanager.SetLibraryController(libcon)
-    usermanager.CreateUser(1000)
-    usermanager.CreateUser(1001)
-
-    return libcon
-
+    usermanager.CreateUser(idnumb.newID())
+    print 'User ID created: ',idnumb.idnumber
+    usermanager.CreateUser(idnumb.newID())
+    print 'User ID created: ',idnumb.idnumber
 
 def exercise1(userid, title, libcon):
     libcon.user_checkout(userid, title)
 
 def main():
-    print "Startup..."
+    print "Initialising library controller..."
+    libcon = library.LibraryController()
+    print "Populating library catalogue..."
+    infile='top100t.txt'
     try:
-        libcon = startup()
+        librarycatalogue(libcon,infile)
     except:
-        print
-        "Startup failed"
+        print "Catalogue populating failed"
+        raise
+    print "Populating library members..."
+    try:
+        librarymembers(libcon)
+    except:
+        print "Catalogue populating failed"
         raise
     print "Exercise 1..."
     try:
-        exercise1(1000, 'Beef Recipes', libcon)
+        exercise1(100, 'The Kite Runner', libcon)
     except:
-        print
-        "Exercise 1 failed"
+        print "Exercise 1 failed"
         raise
 
 if __name__ == '__main__':
