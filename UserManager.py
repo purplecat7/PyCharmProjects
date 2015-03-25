65556075a029bca3bb4415e49cfa4caef9272506
Dan Hodson <d.l.r.hodson@reading.ac.uk>
2015-03-25 15:42:25 +0000

    New User Manager

--- UserManager.py
+++ UserManager.py
@@ -4,4 +4,24 @@ Created on Wed Mar 25 10:41:12 2015
 
 @author: nerc
 """
+import user
+class UserManager():
+    
+    def SetLibraryController(self, library_controller):
+        self.library_controller = library_controller    
+    
+    def CreateUser(self,user_id):
 
+        NewUser=user.User()
+        NewUser.identification(user_id)
+        self.library_controller.add_user(NewUser)
+ #       NewUser._identification = str()
+ #       NewUser._first_name = str()
+ #       NewUser._last_name = str()
+
+        
+
+#    LibraryController.AddUser(NewUser)
+        
+    
+    
