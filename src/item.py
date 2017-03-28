af7029039e7f4300265859ec9a74ea6bf7c17e20
Thomas Eldridge <Thomas Eldridge>
2017-03-28 15:27:00 +0100

    Skeleton of Item class designed. Some code has been added but not checked or tested.

---
+++ src/item.py
@@ -0,0 +1,36 @@
+class Item:
+    """An object representing an item available for lending/borrowing within the library."""
+    def __init__(self, title, ident):
+        """Inputs:
+            - title; a string representing the title of the item
+            - ident; an integer representing the ID number of the item
+        Sets the _checkout_date, _title and _identity attributes of Item.
+        _checkout_date is set as None (until set_checkout method is used),
+        _title is set as the title argument passed in the constructor,
+        _identity is set as the indent argument passed in the constructor."""
+        self._checkout_date = None
+        if type(title) == str:
+            self._title = title
+        else:
+            raise TypeError('title (arg1) should be of type string')
+        if type(ident) == int:
+            self._identity = ident
+        else:
+            raise TypeError('ident (arg2) should be of type int')
+    def __del__(self):
+        """Deletes the Item and its associations."""
+        pass
+    def get_fine_due(self):
+        """Calculates the fine due for this item. Fine is calculated based on the number of
+        days since the due date, multiplied by the fine rate (per day)."""
+    def get_identifier(self, identifier_type):
+        """"""
+    def is_checked_out(self):
+        """Returns True if checked out, returns False otherwise."""
+        return self._checkout_date is not None
+    def reset_checkout(self):
+        """Checks book back in; _checkout_date is restored to None."""
+        self._checkout_date = None
+    def set_checkout(self, date):
+        """Sets the current date as the checkout date"""
+        self._checkout_date = date # I'd like it if the checkout date could be set to datetime.now()
