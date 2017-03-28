1b0d321473b374f11103853de293c37116ca9e3c
Izzy <hippo@ballerina.com>
2017-03-28 17:01:15 +0100

    More complete docstrings and super function

--- src/itemlist.py
+++ src/itemlist.py
@@ -1,9 +1,11 @@
 __author__ = 'nerc'
 from datetime import datetime
 
+
 class ItemList(list):
 
     def __init__(self):
+        super.__init__()
         # initialise attributes
         pass
 
@@ -12,34 +14,36 @@ class ItemList(list):
 
     def add_item(self, new_item):
         """
-
-        :param new_item:
+        Adds an item object
+        :param new_item: item to be added to item list
         :return:
         """
         self.append(new_item)
 
-    def checkout_item(self, item):
+    def checkout_item(self, item_id):
         """
         Finds the requested item and changes the check out date to
         today's date.
-        :param item:
+        :param item_id: ID of item to be checked out
         :return:
         """
-        to_checkout = self.get_item(item)
+        to_checkout = self.get_item(item_id)
         to_checkout.set_checkout(datetime.now())
 
-    def get_fines(self, item):
+    def get_fines(self, item_id):
         """
-        Returns the fines on the item.
+        Finds the item from its ID and returns the fines incurred on item.
+        :param item_id: ID of item to be returned
         :return:
         """
-        fined_item = self.get_item(item)
+        fined_item = self.get_item(item_id)
         fined_item.get_fine_due()
 
     def get_item(self, ID):
         """
-        Finds the requested item
-        :param item: the requested item
+        Finds the requested item from its ID, which can be either
+        an integer or a string
+        :param ID: the requested item
         :return:
         """
         if type(ID) is int:
@@ -53,12 +57,13 @@ class ItemList(list):
             if ID == item.get_identifier(identifier_type):
                 return item
 
-    def is_on_loan(self, item):
+    def is_on_loan(self, item_id):
         """
-        Checks if an item is checked out.
+        Finds item from ID and checks if an item is on loan
+        :param item_id: ID of item to be removed
         :return:
         """
-        loan_item = self.get_item(item)
+        loan_item = self.get_item(item_id)
         loan_item.is_checked_out()
 
     def number_of_items(self):
@@ -68,20 +73,21 @@ class ItemList(list):
         """
         return len(self)
 
-    def remove_item(self, item):
+    def remove_item(self, item_id):
         """
-        Removes an item from the item list
-        :param item: Item to be removed
+        Finds item and removes an item from the item list
+        :param item_id: ID of item to be removed
         :return:
         """
-        remove_item = self.get_item(item)
+        remove_item = self.get_item(item_id)
         self.remove(remove_item)
 
-    def return_item(self, item):
+    def return_item(self, item_id):
         """
-
+        Finds an item from its ID and sets its checkout date to 'None'
+        :param item_id: ID of item to be returned
         :return:
         """
-        returning = self.get_item(item)
+        returning = self.get_item(item_id)
         returning.reset_checkout()
 
