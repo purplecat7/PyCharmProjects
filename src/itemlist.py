c60a8299bb43e86b7b0b8ea08a33169a38b9bc2c
Izzy <hippo@ballerina.com>
2017-03-28 16:19:39 +0100

    Adding itemlist.py list

--- src/itemlist.py
+++ src/itemlist.py
@@ -10,7 +10,7 @@ class ItemList(list):
     def __del__(self):
         pass
 
-    def addItem(self, new_item):
+    def add_item(self, new_item):
         """
 
         :param new_item:
@@ -20,52 +20,59 @@ class ItemList(list):
 
     def checkout_item(self, item):
         """
-
+        Finds the requested item and changes the check out date to
+        today's date.
         :param item:
         :return:
         """
-        to_checkout = next((x for x in self if x._identity == item), None)
+        to_checkout = self.get_item(item)
         to_checkout.set_checkout(datetime.now())
 
-
-    def get_fines(self):
+    def get_fines(self, item):
         """
-
+        Returns the fines on the item.
         :return:
         """
-        pass
+        fined_item = self.get_item(item)
+        fined_item.get_fine_due()
 
-    def get_item(self):
+    def get_item(self, item):
         """
-
+        Finds the requested item
+        :param item: the requested item
         :return:
         """
-        pass
+        return next((x for x in self if x.identity == item), None)
 
-    def is_on_loan(self):
+    def is_on_loan(self, item):
         """
-
+        Checks if an item is checked out.
         :return:
         """
-        pass
+        loan_item = self.get_item(item)
+        loan_item.is_checked_out()
 
     def number_of_items(self):
         """
+        Returns the number of items in item list
+        :return:
+        """
+        return len(self)
+
+    def remove_item(self, item):
+        """
+        Removes an item from the item list
+        :param item: Item to be removed
+        :return:
+        """
+        remove_item = self.get_item(item)
+        self.remove(remove_item)
+
+    def return_item(self, item):
+        """
 
         :return:
         """
-        pass
+        returning = self.get_item(item)
+        returning.reset_checkout()
 
-    def remove_item(self):
-        """
-
-        :return:
-        """
-        pass
-
-    def return_item(self):
-        """
-
-        :return:
-        """
-        pass
