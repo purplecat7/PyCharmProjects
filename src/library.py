e1e6e627476bd277b4a8aaab3c3bf2375bbd1966
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-03-28 15:53:32 +0100

    mixed case to underscores.

--- src/library.py
+++ src/library.py
@@ -1,40 +1,40 @@
-from item_list import ItemList
-from user_list import UserList
+from itemlist import ItemList
+from userlist import UserList
 
 class LibraryController(object):
     MAX_LOANS = 5
     MAX_FINE = 50
 
     def __init__(self):
-        self._itemList = ItemList(self)
-        self._userList = UserList(self)
+        self._item_list = ItemList(self)
+        self._user_list = UserList(self)
 
-    def userCheckout(self, userId, itemTitle):
+    def user_checkout(self, user_id, item_title):
         """Checkout an item for a user."""
-        if self._userList.ableToBorrow(userId, self.MAX_LOANS, self.MAX_FINE):
-            return self._itemList.checkoutItem(itemTitle)
+        if self._userList.able_to_borrow(user_id, self.MAX_LOANS, self.MAX_FINE):
+            return self._item_list.checkout_item(item_title)
 
-    def userReturn(self, userId, itemTitle):
+    def user_return(self, user_id, item_title):
         """Return an item for a user."""
-        self._userList.returnItem(userId)
+        self._userList.return_item(user_id)
 
-    def isOnLoad(self, itemTitle):
+    def is_on_loan(self, item_title):
         """Find out if an item is on load."""
-        print(itemTitle)
+        print(item_title)
 
-    def payFine(self, userId, amount):
+    def pay_fine(self, user_id, amount):
         """Pay a find for a user."""
-        print(userId)
+        print(user_id)
         print(amount)
 
-    def userFind(self, userId):
+    def user_find(self, user_id):
         """Find a user from their ID."""
-        print(userId)
+        print(user_id)
 
-    def addItem(self, item):
+    def add_item(self, item):
         """Add an item to the library."""
         print(item)
 
-    def addUser(self, userId):
+    def add_user(self, user_id):
         """Add an user to the library."""
-        print(userId)
+        print(user_id)
