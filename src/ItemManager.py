02b64ac8444ecf189cd449dbdaa0dcf0ecaf3470
cdunning <c.m.dunning@pgr.reading.ac.uk>
2017-03-28 16:59:41 +0100

    Added comments and doc strings to ItemManager

--- src/ItemManager.py
+++ src/ItemManager.py
@@ -11,17 +11,35 @@ class ItemManager:
         pass
 
     def extractTitles(self,textfile):
-        # Open the text files and extract titles
+        """ This function opens a text file, iterates through each line
+        each title is then added to a list
+        The list is then returned
+        Only the first 100 lines are used"""
+
+        # Open the text file
         f             = open(textfile)
         list_of_items = []
+
+        # Loop through each line and append to a list
         for i in range(0,99):
             list_of_items.append(f.next().strip())
         f.close()
+
         return list_of_items
 
     def createDatabase(self,textfile):
+        """ This function opens a textfile using extract_titles
+        It then loops through the list
+        The first 30 are created as books
+        the next 30 are created as DVDs
+        the last ones are created as Journals
+        This function uses the classes Book,DVD and Journal from itemSubclasses
+        It also uses add_item from _libraryController"""
+
         # Open the text files and extract titles
         title_list = extractTitles(textfile)
+
+        # Loop through the list of titles
         for ind_title, title in enumerate(len(title_list)):
             if ind_title <30:
                 book_instance = Book(title, 'Book_%3i'%(ind_title))
