fbdd39183efecd836e734797aaa291a2162bfe32
Izzy <hippo@ballerina.com>
2017-03-29 15:34:53 +0100

    FineTooHigh renamed FineTooHighError in able_to_borrow

--- test/test_userlist.py
+++ test/test_userlist.py
@@ -12,3 +12,11 @@ def test_able_to_borrow():
     user_list.add_user(ghost_user)
     result = user_list.able_to_borrow(35, 4, 50)
     ns.assert_true(result)
+
+@ns.raises(FineHigh)
+def test_able_to_borrow_fine_fail():
+    ghost_user = User(35)
+    ghost_user._fines = 60.0
+    user_list = UserList()
+    user_list.add_user(ghost_user)
+    result = user_list.able_to_borrow(35, 4, 50)
