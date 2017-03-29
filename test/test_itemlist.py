9897386c793e247fef8488c43fff3b6f1820fc9c
Kjell <k.zumberge@reading.ac.uk>
2017-03-29 15:22:52 +0100

    Added new tests for get_item

--- test/test_itemlist.py
+++ test/test_itemlist.py
@@ -1,5 +1,8 @@
 from src.itemlist import ItemList
 from src.itemSubclasses import Book
+from src.item import Item
+from datetime import datetime
+
 import nose.tools as ns
 
 
@@ -19,3 +22,22 @@ def test_get_identifier_title():
     book = Book('Great Expectations', 1)
     ns.assert_equals(book.get_identifier('Title'), 'Great Expectations')
 
+
+def test_get_item_str():
+    identifier = 'Great Expectations'
+    item_list = ItemList()
+    book = Book('Great Expectations', 1)
+    item_list.add_item(book)
+    item = item_list.get_item(identifier)
+    ns.assert_equals(item, book)
+
+
+def test_get_item_ID():
+    item = ItemList().get_item(1)
+    ns.assert_equals(item, ItemList().add_item(Book('Great Expectations', 1)))
+
+
+def test_checkout_date():
+    date = datetime.now()
+    checkout_date = Item().set_checkout(date)
+    ns.assert_equals(checkout_date, date)
