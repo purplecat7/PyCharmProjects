db3ce9f21b715841e0fdc7a047a65439c03619f1
Izzy <hippo@ballerina.com>
2017-03-29 16:32:59 +0100

    FineTooHigh renamed FineTooHighError in able_to_borrow

--- test/test_userlist.py
+++ test/test_userlist.py
@@ -3,6 +3,7 @@ from src.user import User
 from src.userlist import UserList
 from src.itemlist import ItemList
 from src.itemSubclasses import Book
+import src.library_exceptions as lex
 import nose.tools as ns
 
 
@@ -13,7 +14,7 @@ def test_able_to_borrow():
     result = user_list.able_to_borrow(35, 4, 50)
     ns.assert_true(result)
 
-@ns.raises(FineHigh)
+@ns.raises(lex.FineHighError)
 def test_able_to_borrow_fine_fail():
     ghost_user = User(35)
     ghost_user._fines = 60.0
