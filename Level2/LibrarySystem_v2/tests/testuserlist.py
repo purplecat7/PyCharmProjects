0cc6c7e4e29f76fb1cef33de45c3eabe81a9c63f
purplecat7 <j.p.lewis@reading.ac.uk>
2017-03-30 14:35:49 +0100

    tests added

--- Level2/LibrarySystem_v2/tests/testuserlist.py
+++ Level2/LibrarySystem_v2/tests/testuserlist.py
@@ -1,16 +1,32 @@
-from src import user_list
-from src import userid_error
-from src import user
+from ..src.user_list import UserList
+from ..src.userid_error import UserIdError
+from ..src.user import User
 
-from nose.tools import *
+import nose.tools as ns
 
 
-@raises(userid_error.UserIdError)
+@ns.raises(UserIdError)
 def test_find_user():
-
-    new_user = user.User()
+    new_user = User()
     new_user.set_identification(1)
-
-    ul = user_list.UserList()
+    ul = UserList()
     ul.add_user(new_user)
     ul._find_user(2)
+
+
+def test_able_to_borrow():
+    new_user = User()
+    new_user.set_identification(35)
+    user_list = UserList()
+    user_list.add_user(new_user)
+    result = user_list.able_to_borrow(35, 4, 50)
+    ns.assert_true(result)
+
+
+def test_able_to_borrow_fine_fail():
+    new_user = User()
+    new_user.set_identification(35)
+    new_user._fines = 60.0
+    user_list = UserList()
+    user_list.add_user(new_user)
+    ns.assert_true(user_list.able_to_borrow(35, 4, 50))
