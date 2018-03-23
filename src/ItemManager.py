e88c9e3ba261511c4cca093d2c3a02ab52195f24
purplecat7 <j.p.lewis@reading.ac.uk>
2018-03-23 10:36:48 +0000

    Helpful skeleton code

--- src/ItemManager.py
+++ src/ItemManager.py
@@ -1,1 +1,83 @@
 # TODO write something here
+"""
+NAME
+    item_manager - implementation of ItemManager class
+FILE
+    item_manager.py
+CLASSES
+    ItemManager
+"""
+
+import item
+
+
+class ItemManager:
+    """
+    Class handling the creation of new items
+
+    Methods defined here:
+
+        set_library_controller(...)
+            Store a reference to the LibraryController object
+
+        create_book(...)
+            Create a new 'book' object and pass to the library controller to add to its item list
+
+        create_dvd(...)
+            Create a new 'dvd' object and pass to the library controller to add to its item list
+
+        create_journal(...)
+            Create a new 'journal' object and pass to the library controller to add to its item list
+
+    ----------------------------------------------------------------------
+    No data or other attributes defined here.
+
+    """
+
+    def __init__(self):
+        self._library_controller = None
+
+    def __del__(self):
+        pass
+
+    def set_library_controller(self, library_controller):
+        """
+        Store a reference to the LibraryController object
+
+        :param library_controller: the LibraryController object
+        :return: no return
+        """
+
+    def create_book(self, title, ident):
+        """
+        Create a new 'book' object and pass to the library controller to add to its item list
+
+        :param title: title of the book to be created
+        :param ident: id of the book to be created
+        :return: no return
+        """
+        # create object
+        # tell the library controller about it
+
+    def create_dvd(self, title, ident):
+        """
+        Create a new 'dvd' object and pass to the library controller to add to its item list
+
+        :param title: title of the dvd to be created
+        :param ident: id of the dvd to be created
+        :return: no return
+        """
+        # create object
+        # tell the library controller about it
+
+    def create_journal(self, title, ident):
+        """
+        Create a new 'journal' object and pass to the library controller to add to its item list
+
+        :param title: title of the journal to be created
+        :param ident: id of the journal to be created
+        :return: no return
+        """
+        new_journal = item.Journal(title, ident)
+        # create object
+        # tell the library controller about it
--- src/UserManager.py
+++ src/UserManager.py
@@ -1,3 +1,57 @@
+"""
+NAME
+    user_manager - implementation of ItemManager class
+FILE
+    user_manager.py
+CLASSES
+    UserManager
+"""
+
+import user
+
+
+class UserManager(object):
+    """
+    Class handling the creation of new users
+
+    Methods defined here:
+
+        set_library_controller(...)
+            Store a reference to the LibraryController object
+
+        create_user(...)
+            Create a new 'user' object and pass to the library controller to add to its item list
+
+    ----------------------------------------------------------------------
+    No data or other attributes defined here.
+
+    """
+
+    def __init__(self):
+        self._library_controller = None
+
+    def __del__(self):
+        pass
+
+    def set_library_controller(self, library_controller):
+        """
+        Store a reference to the LibraryController object
+
+        :param library_controller: the LibraryController object
+        :return: no return
+        """
+
+    def create_user(self, user_id):
+        """
+        Create a new 'user' object and pass to the library controller to add to its user list
+
+        :param user_id: unique identifier for the user
+        :return: no return
+        """
+        # create user object and set its id
+        # tell the library controller about it
+
+
 class UserManager:
     # Q: What is a "manager" thing?
     # Q: Should be called "UserCollectionManager"?
