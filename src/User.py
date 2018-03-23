0ff3310b50844b0f286eab9f5ed2edd57199b3a8
fkhunt <freja.hunt@the-iea.org>
2018-03-23 11:26:37 +0000

    Amend checkout and return to get item

--- src/User.py
+++ src/User.py
@@ -31,21 +31,27 @@ class User(object):
         :param date: the date of checkout
         :return: N/A
         """
-        self._ItemList.add_item(item_requested, date)
+        item = self._ItemList.get_item(item_requested)
+        item.set_checkout(date)
 
     def return_item(self, item_id):
         """Remove item from user's item list
         :param item_id: ID of item to be returned
         :return: N/A
         """
-        self._ItemList.return_item(item_id)
+        item = self._ItemList.get_item(item_id)
+        self._ItemList.return_item(item)
 
     def get_fines(self):
         """Check total fines for user
         :param: none
         :return: total fine owed by user
         """
-        return self._ItemList.get_fines() + self._fines
+        items = self._ItemList._items
+        fine = 0
+        for item in items:
+            fine = fine + item.get_fine_due()
+        return fine + self._fines
 
     def pay_fine(self, amount):
         """Subtract paid amount from total
