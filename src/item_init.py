112cff786856288779d6d5945a81fc81d1b05b29
S-hannon <s.jones2@pgr.reading.ac.uk>
2018-06-04 14:59:04 +0100

    Done?

--- src/item_init.py
+++ src/item_init.py
@@ -1,18 +1,27 @@
+import item
+
 class ItemInit():
     """Adds stuff."""
     
     def __init__(self):
+        self.lib_mgr = None
         
-    def set_library_manager(self, manager):
+    def set_library_manager(self, library):
         """sets library manager"""
+        self.lib_mgr = library
 
-        
-    def create_book(self, name):
+    def create_book(self, name, id):
         """Adds a book to item list."""
+        theitem = item.Book(name, id)
+        self.lib_mgr.add_item(theitem)
         
-    def create_dvd(self, name):
+    def create_dvd(self, name, id):
         """Adds a dvd to item list."""
+        theitem = item.Dvd(name, id)
+        self.lib_mgr.add_item(theitem)
     
-    def create_journal(self, name):
+    def create_journal(self, name, id):
         """Adds a journal to item list."""
+        theitem = item.Journal(name, id)
+        self.lib_mgr.add_item(theitem)
         
