22391ecf4ff1675ca78e79c23b38e4a7583339a3
jraylmer <32389493+jraylmer@users.noreply.github.com>
2018-06-04 14:54:42 +0100

    Add some methods to item class.

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+from __future__ import division
+from datetime import datetime
+
 class Item():
     """
     Class for items in the library.
@@ -6,18 +9,31 @@ class Item():
         id            : int, unique identifier for the item.
         title         : str, title of item.
         checkout_date : date at which item was checked-out.
+        checked_out   : bool, whether checked out or not.
 
     Methods:
-        get_fine()       : calculate the fine owed on this item.
-        set_checkout()   : set the date of checkout
-        clear_checkout() : set the item to be not checked-out (i.e. check-in).
+        get_identifier()     : return the ID number attribute.
+        get_title()          : return the title attribute.
+        get_checkout_date () : return the checkout date attribute.
+        get_fine_due()       : calculate the fine owed on this item.
+        is_available()       : return True if available, False otherwise.
+        set_checkout()       : set the date of checkout to today's date.
+        clear_checkout()     : set the item to be not checked-out (i.e. check-
+                               in) and clear checkout_date attribute.
 
     """
 
     def __init__(self, id, title, checkout_date):
+        """Initialise instance variables.
+
+        id : ID number.
+        title: title of item.
+        checkout_date: datetime-module date when checked out.
+        """
         self.id = id
         self.title = title
         self.checkout_date = checkout_date
+        self.checked_out = True
         pass
 
     def get_identifier(self):
@@ -34,15 +50,23 @@ class Item():
 
     def get_fine_due(self):
         """Calculate the fine owed on this item."""
-        the_fine = 0.0
+        days_overdue = datetime.today() - (self.checkout_date + self.max_loan_time)
+        the_fine = self.fee_rate*days_overdue*(days_overdue>0)
         return the_fine
+
+    def is_available(self):
+        return not(self.checked_out == True)
 
     def set_checkout(self):
         """Set the checkout date somehow."""
+        self.checkout_date = datetime.today()
+        self.checked_out = True
         pass
 
     def clear_checkout(self):
         """Clear the checkout date somehow."""
+        self.checkout_date = None
+        self.checked_out = False
         pass
 
 
