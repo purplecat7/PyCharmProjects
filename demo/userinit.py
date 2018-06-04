3ac7d284f7c47ff0de9125975a10cf1ce36b82bc
Agnieszka <agnieszka.walenkiewicz@gmail.com>
2018-06-04 13:48:15 +0100

    first commit of user init file

---
+++ demo/userinit.py
@@ -0,0 +1,25 @@
+class UserInit():
+    """
+    This is an initialization class.
+    This class needs to know about the Library Manager
+    and the Library Manager needs to know about this class.
+    User Init class methods: allow to create a user
+    that has an ID, name and surname
+    """
+    def __init__(self):
+        pass
+
+    def setLibManager(theLibrary):
+        """
+        Two classes User Init and Library Manager
+        need to know about each other
+        :return:
+        """
+        self.LibManager = theLibrary
+
+    def create_user(self):
+        """
+        Create a user with name, surname and ID.
+        Allow the Library Manager to add the user to the user list
+        :return:
+        """
