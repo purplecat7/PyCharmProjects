7d045590630913b126edd49f5eb17846b2b4569b
S-hannon <s.jones2@pgr.reading.ac.uk>
2018-06-04 16:13:46 +0100

    change back

--- src/item_init.py
+++ src/item_init.py
@@ -10,18 +10,18 @@ class ItemInit():
         """sets library manager"""
         self.lib_mgr = library
 
-    def create_book(self, id, name):
+    def create_book(self, name, id):
         """Adds a book to item list."""
-        theitem = item.Book(id, name)
+        theitem = item.Book(name, id)
         self.lib_mgr.add_item(theitem)
         
-    def create_dvd(self, id, name):
+    def create_dvd(self, name, id):
         """Adds a dvd to item list."""
-        theitem = item.Dvd(id, name)
+        theitem = item.Dvd(name, id)
         self.lib_mgr.add_item(theitem)
     
-    def create_journal(self, id, name):
+    def create_journal(self, name, id):
         """Adds a journal to item list."""
-        theitem = item.Journal(id, name)
+        theitem = item.Journal(name, id)
         self.lib_mgr.add_item(theitem)
         
