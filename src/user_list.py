93389f8b7e52903e0153e85bef9af068ac6ed059
Rebecca Atkinson <becca.atkinson@hotmail.co.uk>
2018-06-05 10:26:12 +0100

    Pep8 standards for user_list

--- src/user_list.py
+++ src/user_list.py
@@ -8,20 +8,28 @@ Has methods to:
 
 from user import User
 
+
 class UserList:
     """
-    This class is a list of all the users in the library system.
+    A class for a list of users.
 
-    It has methods to:
-        Add a new user to the list.
-        Find a user on the list from its ID
+    Attributes:
+        user_list             : list, contains all the user objects
+
+
+    Methods:
+        add_user             : adds a user to the list
+        find                 : returns a user from the list given their ID
+        able_to_borrow       : return bool specifying whether the user can borrow a new item
+        checkout             : passes checkout message to a user on the list
+        return_item          : passes return message to a user on the list
 
     """
 
     def __init__(self):
         # create a list
-        self.userlist = list()
-
+        self.user_list = list()
+        pass
 
     def __del__(self):
         pass
@@ -29,58 +37,65 @@ class UserList:
     def add_user(self, user):
         """
         Add a new user item to the list
-        :param user:
-        :return:
+
+        :param user: user object, to be added to the list
         """
-        #add the user to the list
-        self.userlist.append(user)
+        # add the user to the list
+        self.user_list.append(user)
+        pass
 
     def find(self, id):
         """
         Find the user from the ID on the list
-        :param id:
-        :return: the item with that ID
+
+        :param id: int, id of a user to find on the list
+        :return: User object or None if no user with that id is on the list
         """
-        #for each item in the list check its ID
+        # for each item in the list check its ID
         # and return the user whose ID matches the ID provided or None if user doesn't exist
-        for user in self.userlist:
+        for user in self.user_list:
             if id == user.get_id():
                 return user
         else:
-            return False
+            return None
 
-    def able_to_borrow(self, id, maxfines, maxallowed):
+    def able_to_borrow(self, user_id, max_fines, max_allowed):
         """
         Find if a user is able to borrow a new item.
-        :param id:
-        :return: bool
-        """
-        #Find the Item on the list from the ID
-        user = self.find(id)
-        #check its a valid id throw exception if not
-        #ask the user if it can borrow
-        return user.able_to_borrow(maxfines, maxallowed)
 
-    def checkout(self, id, item):
+        :param user_id: int, id of a user to find on the list
+        :param max_fines: float, max amount of fines allowed
+        :param max_allowed: int, max amount of items allowed checked out
+        :return: bool specifying whether a user may borrow
+        """
+        # Find the Item on the list from the ID
+        user = self.find(user_id)
+        # check its a valid user_id (could throw exception if not)
+        # ask the user if it can borrow
+        return user.able_to_borrow(max_fines, max_allowed)
+
+    def checkout(self, user_id, item):
         """
         Checkout the book to a user on the list.
-        :param id:
-        :param item:
+
+        :param user_id: an int giving the id of a user to find on the list
+        :param item: an item object for the user to checkout
         :return: calls checkout on the user with item
         """
-        #Find the user on the list
-        user = self.find(id)
-        #tell user to checkout item
+        # Find the user on the list
+        user = self.find(user_id)
+        # tell user to checkout item
         return user.checkout(item)
 
-    def return_item(self, id, item):
+    def return_item(self, user_id, item):
         """
         Return the an item in a users account.
-        :param id:
-        :param Item:
+
+        :param user_id: an int giving the id of a user to find on the list
+        :param item: an item object for the user to checkout
         :return: returns item that user (with id) has out.
         """
-        #Find the user in the list
-        user = self.find(id)
-        #Tell user to return the item.
+        # Find the user in the list
+        user = self.find(user_id)
+        # Tell user to return the item.
         return user.return_item(item)
