3326335cd1260c96914291421a14a59943bf0cca
Agnieszka <agnieszka.walenkiewicz@gmail.com>
2018-06-10 23:41:35 +0100

    best shot at adding docstring (PEP8 coding standards)

--- src/userinit.py
+++ src/userinit.py
@@ -1,33 +1,58 @@
 
 
 import user
-#import library_manager as m
+# import library_manager as m
 
-class UserInit():
+
+class UserInit:
     """
-    This is an initialization class.
+    This is the user initializer class.
     This class needs to know about the Library Manager
     and the Library Manager needs to know about this class.
-    User Init class methods: allow to create a user
-    that has an ID, name and surname
+    -------
+    Attributes:
+    None
+    -------
+    Methods:
+    set_library controller:     sets pointer to library manager
+    create_user:                creates user's ID
     """
     def __init__(self):
+        """
+        Equivalent to the constructor. Called when class created.
+        --------
+        :param n/a
+        :return n/a
+        """
         self.lib_mgr = None     # holds pointer to library manager
+        pass
+
+    def __del__(self):
+        """
+        Equivalent of the destructor. Called on instance
+        deletion, clears memory.
+        ----------
+        :param n/a
+        :return n/a
+        """
         pass
 
     def set_library_controller(self, theLibrary):
         """
-        Two classes User Init and Library Manager
-        need to know about each other
-        :return:
+        Sets pointer to the address in the memory where
+        the library manager is.
+        --------
+        :param library manager
+        :return n/a
         """
         self.lib_mgr = theLibrary
 
     def create_user(self, id):
         """
-        Create a user with name, surname and ID.
-        Allow the Library Manager to add the user to the user list
-        :return:
+        Creates user's id.
+        ---------
+        :param ID (of the user, from main)
+        :return create user's ID
         """
         theuser = user.User(id)
         self.lib_mgr.add_user(theuser)
