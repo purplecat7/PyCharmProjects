856616fc928a69153ddf0b7f7a531e698876d232
purplecat7 <j.p.lewis@reading.ac.uk>
2018-06-11 12:15:09 +0100

    Skeleton code with docstrings

---
+++ src_skeleton/user_manager.py
@@ -0,0 +1,54 @@
+"""
+NAME
+    user_manager - implementation of ItemManager class
+FILE
+    user_manager.py
+CLASSES
+    UserManager
+"""
+
+import user
+
+
+class UserManager(object):
+    """
+    Class handling the creation of new users
+
+    Methods defined here:
+
+        set_library_controller(...)
+            Store a reference to the LibraryController object
+
+        create_user(...)
+            Create a new 'user' object and pass to the library controller to add to its item list
+
+    ----------------------------------------------------------------------
+    No data or other attributes defined here.
+
+    """
+    def __init__(self):
+
+
+    def __del__(self):
+        pass
+
+    def set_library_controller(self, library_controller):
+        """
+        Store a reference to the LibraryController object
+
+        :param library_controller: the LibraryController object
+        :return: no return
+        """
+    
+    def create_user(self, user_id):
+        """
+        Create a new 'user' object and pass to the library controller to add to its user list
+
+        :param user_id: unique identifier for the user
+        :return: no return
+        """
+
+
+        
+    
+    
