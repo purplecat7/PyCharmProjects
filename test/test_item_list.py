e01941a1be5242e6d1a188dacd321746e3d24b9e
Bethan Harris <blharris24@gmail.com>
2018-06-11 12:40:04 +0100

    Fix tests for ItemList class

--- test/test_item_list.py
+++ test/test_item_list.py
@@ -1,6 +1,7 @@
 from src import item_list, item
 import nose.tools as ns
 from src import except_item_not_available
+from __future__ import print_function
 
 
 class TestItemList(object):
@@ -21,9 +22,9 @@ class TestItemList(object):
         """This method is run once before _each_ test method is executed"""
         # use self.attribute to keep anything which needs to be accessed later
         print('setUp method\n')
-        my_list = item_list.ItemList()
-        my_item = item.Item(34, 'Cloud Atlas')
-        my_list.add_item(my_item)
+        self.my_list = item_list.ItemList()
+        self.my_item = item.Item(34, 'Cloud Atlas')
+        self.my_list.add_item(self.my_item)
 
 
     def tearDown(self):
@@ -33,19 +34,19 @@ class TestItemList(object):
     def test_number_of_items(self):
         print('test_number_of_items\n')
         my_second_item = item.Item(31, 'City of Stairs')
-        my_list.add_item(my_second_item)
-        ns.assert_equals(my_list.number_of_items(), 2)
+        self.my_list.add_item(my_second_item)
+        ns.assert_equals(self.my_list.number_of_items(), 2)
 
     def test_checkout(self):
         print('test_checkout\n')
         my_user_list = item_list.ItemList()
-        my_user_list.checkout(my_item)
+        my_user_list.checkout(self.my_item)
         ns.assert_equals(my_user_list.number_of_items(), 1)
 
     @ns.raises(except_item_not_available.ItemNotAvailableError)
     def test_raise_error_not_available(self):
         print('test_raise_error_not_available\n')
         my_user_list = item_list.ItemList()
-        my_user_list.checkout(my_item)
+        my_user_list.checkout(self.my_item)
         another_user_list = item_list.ItemList()
-        another_user_list.checkout(my_item)
+        another_user_list.checkout(self.my_item)
