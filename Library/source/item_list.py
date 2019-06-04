ab953df4fc75ef4fadc472db86d82248ee38fba5
Laura Merritt <l.e.a.merritt@pgr.reading.ac.uk>
2019-06-04 16:16:20 +0100

    Started writing ItemList class - added methods len_items and is_overdue

---
+++ Library/source/item_list.py
@@ -0,0 +1,21 @@
+class ItemList(list):
+    """
+    This is a list that holds a number of items
+    methods:
+    len_items()
+    is_overdue(item)
+    add_to_list()
+    get_item()
+    find_items()
+    """
+    def len_items(self):
+        '''
+        number of items in the list
+        :return: length of self
+        '''
+
+    def is_overdue(self, item):
+        '''
+        asks the
+        :return:
+        '''
