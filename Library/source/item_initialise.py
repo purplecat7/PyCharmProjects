a6fbd807ff34a7232d78b0fe0200c5d95dea695f
JakeBland <j.s.bland@pgr.reading.ac.uk>
2019-06-04 16:34:47 +0100

    Some scenarios in main

---
+++ Library/source/item_initialise.py
@@ -0,0 +1,13 @@
+
+class ItemInitialise():
+
+    def __init__(self):
+
+        pass
+
+    def __del__(self):
+
+        pass
+
+    def load_items(self, file, itemlist):
+
--- Library/source/main.py
+++ Library/source/main.py
@@ -1,4 +1,5 @@
-
+from libsys import LibrarySystem
+import datetime
 
 class NumbID:
     """
@@ -29,3 +30,47 @@ class NumbID:
     @staticmethod
     def reset_id():
         NumbID.id_number = 0
+
+
+
+def setup_libsys(filename = False):
+
+    libsys = LibrarySystem()
+    # instantiate Library System
+
+    if filename:
+
+        libsys.load_items(filename)
+        # load items from specified file to the Library System
+
+
+def add_new_item(libsys, item_type, name):
+
+    libsys.load_new_item(item_type, name)
+
+
+def borrow_item(libsys, username, user_id, item_name, item_id, date = datetime.date.today()):
+
+    libsys.borrow_item(username, user_id, item_name, item_id, date)
+
+
+def scenario1(libsys):
+
+    borrow_item(libsys, "JohnnyCodewarrior", None, "Document, Your job depends on it", None)
+
+
+def return_item(libsys, username, user_id, item_name, item_id, date = datetime.date.today()):
+
+    libsys.return_item(username, user_id, item_name, item_id, date)
+
+
+def scenario2(libsys, overdueJournalName):
+
+    return_item(libsys, "JudyHacker", None, overdueJournalName, None)
+    borrow_item(libsys, "JudyHacker", None, "Debugging to music", None)
+
+
+def scenario3(libsys):
+
+
+
