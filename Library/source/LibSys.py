d7ce88e4c8a713d887649c8c906aa337266443ee
emanuele22 <51321984+emanuele22@users.noreply.github.com>
2019-06-10 11:35:53 +0100

    Update and rename LibSys

---
+++ Library/source/LibSys.py
@@ -0,0 +1,29 @@
+class libsys:
+    
+    """
+    Library System Management
+    """
+    
+    def __init__(self, Itemlist, UserList):
+        """
+        Constructor of libsys class
+        """
+        self.myitemlist = ItemList() #list of items
+        self.myuserlist = UserList() #list of users
+        
+    def add_item(self, item):
+        """
+        Add item to itemlist.
+        :param item: instance of item
+        
+        """
+        myitemList.add_item(item)
+        
+    def add_user(self, user):
+        """
+        Add user to user list.
+        :param user: instance of user
+        """
+        myuserList.add_user(user)
+
+        
