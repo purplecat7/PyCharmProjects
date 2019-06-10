ded78fd78a8c443f33767bb06092dc13da7c1123
Emanuele Gentile <e.gentile@pgr.reading.ac.uk>
2019-06-10 15:13:45 +0100

    add return

--- Library/source/LibSys.py
+++ Library/source/LibSys.py
@@ -1,12 +1,12 @@
 from user_list import UserList
 from item_list import ItemList
-
+import datetime
 class LibrarySystem:
-    
+
     """
     Library System Management
     """
-    
+
     def __init__(self):
         """
         Constructor of libsys class
@@ -18,10 +18,10 @@ class LibrarySystem:
         """
         Add item to itemlist.
         :param item: instance of item
-        
+
         """
         self.myitemList.add_item(item)
-        
+
     def add_user(self, user):
         """
         Add user to user list.
@@ -29,37 +29,36 @@ class LibrarySystem:
         """
         self.myuserList.add_user(user)
 
-    def checkout(self, user, itemid):
+    def checkout(self, user, itemid, date):
         """
         Accepts user and id of item from input list
         and passes them to user list
         :param user: user
-        :param itemid: id of iem
+        :param itemid: id of item
         :return: user, itemid
         """
 
         if self.myuserList.can_borrow(user):
             the_item = self.myitemlist.get_item(itemid)
+            the_item.setdate(date=datetime.date.today())
             self.myuserlist.checkout_item(user, the_item)
 
         # JB here you need to give the user_id to the user_list so they can fin the user & check in the book
         # also might want the option to use a different date so that books can be assigned already overdue (possibly with a default value of datetime.date.today())
 
         return user, itemid
-"""
+
     def return_item(self, user, itemid):
-
-        
-        Accepts user and id of item from input list
-        and passes them to item initialiser
+        """
+        Checks in item when returned
         :param user: user
-        :param itemid: id of iem
+        :param itemid: id of item
         :return: user, itemid
-        
+        """
 
-        if self.myuserList.can_borrow(user):
-            the_item = self.myitemlist.get_item(itemid)
-            self.myuserlist.checkin_item(user, the_item)
+        the_item = self.myitemlist.get_item(itemid)
+
+        self.myuserlist.checkin_item(user, the_item)
 
         # JB here you need to give the user_id to the user_list so they can fin the user & check in the book
         # also might want the option to use a different date so that books can be assigned already overdue (possibly with a default value of datetime.date.today())
@@ -67,11 +66,6 @@ class LibrarySystem:
         return user, itemid
 """
 
-# the following are just some ideas of methods we might want in LibSys, JB
-        # feel free to delete these if it's just annoying
-
-#return_item(user_identifier, item_identifier)
-        # user_list
 
 #change_fine_of_user(fine_reduce_by, user_identifier)
 
