611fc5c82b1a7938566f0e52f8be3cae6dc1c0c2
purplecat7 <j.p.lewis@reading.ac.uk>
2020-11-14 13:17:44 +0000

    Updates

--- Level1/6_UsingNose/some_code/functions.py
+++ Level1/6_UsingNose/some_code/functions.py
@@ -16,11 +16,28 @@ def my_func(the_value):
 def my_other_func(val1, val2):
     return val1 + val2
 
+def my_array_func(the_array):
+    try:
+        result = 1
+        for num in range(0, len(the_array)):
+            result = the_array[num] * result
+        return result
+    except TypeError as te:
+        print te.message
+        raise te
+
+
 def main():
     if (my_func(1) == True) :
         print ('Yay, it works!')
     else:
         print ('boo')
+
+    try:
+        data=['a','b','c']
+        print my_array_func(data)
+    except TypeError:
+        print 'User is a muppet'
 
 if __name__ == '__main__':
     main()
--- Level1/6_UsingNose/some_tests/tests.py
+++ Level1/6_UsingNose/some_tests/tests.py
@@ -14,13 +14,21 @@ def test_my_func_true():
     result = f.my_func(2)
     ns.assert_true(result)
 
-
 # TODO you can find this in the 6:TODO view pane
 def test_my_func_false():
     result = f.my_func(1)
     ns.assert_false(result)
 
-
 def test_my_other_func():
     result = f.my_other_func(2, 3)
-    ns.assert_equals(result, 6)
+    ns.assert_equals(result, 5)
+
+def test_my_array_func_integers():
+    data=[1, 2, 3]
+    result = f.my_array_func(data)
+    ns.assert_equal(result, 6)
+
+@ns.raises(TypeError)
+def test_my_array_func_letters():
+    data=['a','b','c']
+    f.my_array_func(data)
