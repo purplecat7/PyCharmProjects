befc480bbf280f89d2fc91da599f7061eab68881
Jane <jane@optaero.com>
2022-08-24 15:15:03 +0100

    Useful number generator

--- src/main.py
+++ src/main.py
@@ -4,6 +4,36 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+class NumbID:
+    """
+    Utility class to provide unique identifier.
+
+    Methods defined here:
+        new_id(...)
+            Provide next unique identifier, starts at 1.
+
+        reset_id(...)
+            Reset unique identifier.
+    """
+    id_number = 0
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    # note that these use the @staticmethod adornment since no instance
+    # attributes are used.
+    @staticmethod
+    def new_id():
+        NumbID.id_number += 1
+        return NumbID.id_number
+
+    @staticmethod
+    def reset_id():
+        NumbID.id_number = 0
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
