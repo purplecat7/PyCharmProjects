bac66bf8fe0108baea271b139ef056d455021eff
LauraRisley <l.risley@pgr.reading.ac.uk>
2022-08-24 15:09:56 +0100

    Updated user file.

--- src/user.py
+++ src/user.py
@@ -1,3 +1,20 @@
 """
 File contains the user class for the alexandria library
 """
+
+class UserClass:
+
+    # class attributes
+    def __init__(self): #constructor
+        """
+        Attibutes of the class
+        """
+        # initialise instance attributes
+        # ID, name
+        self.__user_ID : str
+        self.__user_name : str
+
+
+    # class methods
+
+    # adding fines to the accumulative pot
