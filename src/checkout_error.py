fe12ad7e435fd7b21a164982827af543bd2b3fc6
ku820903 <a.gainford@pgr.reading.ac.uk>
2022-08-25 15:54:49 +0100

    changed checkout errors

--- src/checkout_error.py
+++ src/checkout_error.py
@@ -1,8 +1,16 @@
 
-class CheckoutError(Exception):
+class MaxLoansExceededError(Exception):
 
     def __init__(self):
-        self.message = "Checkout error. Please check max loans and max fines."
+        self.message = "Cannot checkout. User has exceeded the maximum number of loans"
+
+    def __str__(self):
+        return repr(self.message)
+
+class MaxFinesExceededError(Exception):
+
+    def __init__(self):
+        self.message = "Cannot checkout. User has exceeded the maximum fines"
 
     def __str__(self):
         return repr(self.message)
